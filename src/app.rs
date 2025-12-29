b7715690221114d1b8e87945cded43d442998354
Aaron Hernan <aaronhernan@gmail.com>
2025-12-29 14:44:57 -0700

    dual port listening test

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::post,};
+use axum::{Router, routing,};
 use deadpool_redis::{Config as RedisConfig, Runtime};
 use tokio::net::TcpListener;
 use tracing::info;
@@ -27,8 +27,8 @@ pub struct AppState {
 
 pub fn build_router(state: AppState) -> Router {
     Router::new()
-        .route("/webhook",post(handlers::webhook::webhook_handler),)
-        .route("/message",post(handlers::message::message_handler),)
+        .route("/webhook",routing::post(handlers::webhook::webhook_handler),)
+        .route("/message",routing::post(handlers::message::message_handler),)
         .route_layer(axum::middleware::from_fn_with_state(
             state.clone(),
             middleware::webhook_auth,
@@ -36,16 +36,24 @@ pub fn build_router(state: AppState) -> Router {
         .with_state(state)
 }
 
+pub fn build_public_router(state: AppState) -> Router {
+    Router::new()
+        .route("/", routing::get(|| async {"Public route"}))
+        .with_state(state)
+}
+
 //pub async fn run(config: Config) -> Result<(), std::io::Error> {
 pub async fn run(config: Config) -> anyhow::Result<()> {
-    let addr = format!("{}:{}", config.listen_host, config.listen_port);
-    let listener = TcpListener::bind(addr).await?;
+    let addr_private = format!("{}:{}", config.listen_host, config.listen_port);
+    let addr_public = format!("{}:{}", config.listen_host, config.listen_port+1);
+    let listener_private = TcpListener::bind(addr_private).await?;
+    let listener_public = TcpListener::bind(addr_public).await?;
 
-    run_with_listener(listener, config).await
+    run_with_listener(listener_private, listener_public, config).await
 }
 
 //pub async fn run_with_listener( listener: TcpListener, config: Config,) -> Result<(), std::io::Error> {
-pub async fn run_with_listener( listener: TcpListener, config: Config,) -> anyhow::Result<()> {
+pub async fn run_with_listener( listener_private: TcpListener, listener_public: TcpListener, config: Config,) -> anyhow::Result<()> {
     // Creacion del pool de conexiones a Redis
     let redis_cfg = RedisConfig::from_url(&config.redis_url);
     //let mut redis_cfg = RedisConfig::from_url(&config.redis_url);
@@ -67,13 +75,19 @@ pub async fn run_with_listener( listener: TcpListener, config: Config,) -> anyho
     // Comentario de aprendizaje. Vamos a meter el ownership de los elementos en AppState
     //   el state es el dueno de sus dependencias
     let state = AppState { config, evolution, idempotency, message_repository, };
-    let app = build_router(state);
-
+    let app = build_router(state.clone());
+    let public_app = build_public_router(state);
     //let current_filter = tracing::metadata::LevelFilter::current();
     //print!("Current filter: {:?}", current_filter);
 
     info!("Escuchando en http://{}", listener.local_addr()?);
+    //axum::serve(listener, app).await?;
 
-    axum::serve(listener, app).await?;
+    let server_private = tokio::task::spawn(async move {
+        axum::serve(listener, app).await.expect("Error al iniciar servidor private");
+    });
+    let server_public = tokio::task::spawn(async move {
+        axum::serve(listener, app).await.expect("Error al iniciar servidor private");
+    });
     Ok(())
 }
